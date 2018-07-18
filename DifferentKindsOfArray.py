5b5f794636d932587f7a14d243495b923a35e654
DeepaThangaraj <37324192+DeepaThangaraj@users.noreply.github.com>
2018-07-18 17:30:25 +0300

    Creates Differen kind of Array using numpy array

---
+++ DifferentKindsOfArray.py
@@ -0,0 +1,16 @@
+import numpy as np
+#print  Zeros in array
+print("Zeros Array",np.zeros((4,4)))
+
+#print one in array
+print("ones Array",np.ones((5,5)))
+
+#print eigen array
+print("Eigen Array",np.eye(6))
+
+#print diagonal array
+print("Diagonal Array",np.diag(np.array([1,2,3,4,5])))
+
+#print random number of array
+
+print("Random Array",np.random.rand(10)) # uniform in[0,1]
