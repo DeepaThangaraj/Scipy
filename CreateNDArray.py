b91bdb15b917a535402ebb434ab26582b89da77f
DeepaThangaraj <37324192+DeepaThangaraj@users.noreply.github.com>
2018-07-18 17:05:03 +0300

    CreateNDArray
    using NumpyArrays

---
+++ CreateNDArray.py
@@ -0,0 +1,13 @@
+import numpy as np
+# print one Dimensional array
+oneDarray=np.array([0,1,2,3])
+print(oneDarray)
+#print two Dimensional array
+twoDarray=np.array([[2,2,2],[3,3,3]])
+print(twoDarray)
+#print three dimensional array as Data type as list
+threeDArray=np.array([[8,6,4,2],[9,7,5,3,1],[1,3,5,7,9]])
+print(threeDArray)
+# print three dimensional array 
+threeDA=np.array([[2],[3],[4]])
+print(threeDA)
